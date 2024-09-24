627653459f5b55da76d8e6d0ddf0e14a85519616
Ashish-Gupta7 <ash300mr@gmail.com>
2024-09-24 20:36:18 +0530

    props and props drilling

---
+++ 8propsAndProps-drilling/src/components/Component2.jsx
@@ -0,0 +1,11 @@
+const Component2 = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>c2</h1>
+    </div>
+  );
+};
+
+export default Component2;
---
+++ 8propsAndProps-drilling/src/components/Component3.jsx
@@ -0,0 +1,5 @@
+const Component3 = () => {
+  return <div></div>;
+};
+
+export default Component3;
